31677802fbc459e442210e8dd078dd902700a64c
reza2boyce <reza2boyce@gmail.com>
2022-02-08 14:03:01 +0330

    style is maked

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ajax sended data</title>
+    <link rel="stylesheet" href="style/general.css">
+</head>
+<body>
+    <h2 id="title">POST XMLHttpRequest - JS - dcode</h2>
+    <div id="demo">
+
+    </div>
+</body>
+    <script type="text/javascript">
+        
+    </script>
+
+</html>
