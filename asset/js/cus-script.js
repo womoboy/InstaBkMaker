ec9d2b1d80e5d0c58fd8164f2e8bc9008ed69ad4
reza2boyce <reza2boyce@gmail.com>
2022-02-19 15:54:26 +0330

    finish beta version

--- asset/js/cus-script.js
+++ asset/js/cus-script.js
@@ -1,15 +1,11 @@
 //global variable
 var logo_source = "asset/library/Logo_DornaGame.jpg";
-var method_style = [
-    "asset/style/method_01.css",
-    "asset/style/method_02.css",
-    "asset/style/method_03.css"
-];
+
 
 //inputs
 var code = document.getElementById('code');
 var battlePass = document.getElementById('battle-pass');
-var rigen = document.getElemetnById('rigen-list');
+var rigen = document.getElementById('rigen-list');
 var epicGun = document.getElementById('epic-gun');
 var legendGun = document.getElementById('legend-gun');
 var price = document.getElementById('price');
@@ -47,27 +43,35 @@ function action(){
 }
 
 function startInformation(){
+    goMethod();
     video_frame.play();
 }
 
 function goMethod(){
+    var method_style = [
+        "asset/style/method_01.css",
+        "asset/style/method_02.css",
+        "asset/style/method_03.css"
+    ];
+
     if(method.value == 'method-none'){
         header.innerText = no_method_txt;
         footer.innerText = no_method_txt;
     }else if(method.value == 'method-1'){
         cleanContent();
-        let method = new CreateMethod(logo_source, code.value, rigen.vlaue, epicGun.value, legendGun.value, price.value, method_style[0]);
+        let method = new CreateMethod(logo_source, code.value, battlePass.value, rigen.value, epicGun.value, legendGun.value, price.value, method_style[0]);
     }else if(method.value == 'method-2') {
         cleanContent();
-        let method = new CreateMethod(logo_source, code.value, rigen.vlaue, epicGun.value, legendGun.value, price.value, method_style[1]);
+        let method = new CreateMethod(logo_source, code.value, battlePass.value, rigen.value, epicGun.value, legendGun.value, price.value, method_style[1]);
     }else if(method.value == 'method-3') {
         cleanContent();
-        let method = new CreateMethod(logo_source, code.value, rigen.vlaue, epicGun.value, legendGun.value, price.value, method_style[2]);
+        let method = new CreateMethod(logo_source, code.value, battlePass.value ,rigen.value, epicGun.value, legendGun.value, price.value, method_style[2]);
     }
 }
 
 function cleanContent(){
     header.innerHTML = "";
+    footer.innerHTML = "";
 }
 //classes
 class CreateMethod{
@@ -79,7 +83,7 @@ class CreateMethod{
         this.rigen = $rigen;
         this.epicGun = $epicGun;
         this.legendGun = $legendGun;
-        this.proce = $price;
+        this.price = $price;
         this.style = $style;
 
         //select header element
@@ -106,7 +110,13 @@ class CreateMethod{
         //__code text
         var code_text_creator = document.createElement("p");
         code_text_creator.setAttribute("id", "code-text");
-        code_text_creator.innerText = "کد :" + " " + this.code;
+        if(this.code == ""){
+            this.code = 0;
+            code_text_creator.innerText = "کد :" + " " + this.code;
+        } else {
+            code_text_creator.innerText = "کد :" + " " + this.code;
+        }
+        
 
         code_frame_creator.appendChild(code_text_creator);
 
@@ -118,7 +128,13 @@ class CreateMethod{
         //__battlePass text
         var battlePass_text_creator = document.createElement("p");
         battlePass_text_creator.setAttribute("id", "battlePass-text");
-        battlePass_text_creator.innerText = "بتل پس :" + " " + this.battlePass;
+        if(this.battlePass == ""){
+            this.battlePass = 0;
+            battlePass_text_creator.innerText = "بتل پس :" + " " + this.battlePass;
+        } else {
+            battlePass_text_creator.innerText = "بتل پس :" + " " + this.battlePass;
+        }
+        
 
         battlePass_frame_creator.appendChild(battlePass_text_creator);
 
@@ -130,7 +146,12 @@ class CreateMethod{
         //rigen text
         var rigen_text_creator = document.createElement("p");
         rigen_text_creator.setAttribute("id", "rigen-text");
-        rigen_text_creator.innerText = "ریجن :" + " " + this.rigen;
+        if(this.rigen == ""){
+            this.rigen = 0;
+            rigen_text_creator.innerText = "ریجن :" + " " + this.rigen;
+        } else {
+            rigen_text_creator.innerText = "ریجن :" + " " + this.rigen;
+        }
 
         rigen_frame_creator.appendChild(rigen_text_creator);
 
@@ -142,7 +163,12 @@ class CreateMethod{
         //__epicGun text
         var epicGun_text_creator = document.createElement("p");
         epicGun_text_creator.setAttribute("id", "epicGun-text");
-        epicGun_text_creator.innerText = "گان اپیک :" + " " + this.epicGun;
+        if(this.epicGun == ""){
+            this.epicGun = 0;
+            epicGun_text_creator.innerText = "گان اپیک :" + " " + this.epicGun;
+        } else {
+            epicGun_text_creator.innerText = "گان اپیک :" + " " + this.epicGun;
+        }
 
         epicGun_frame_creator.appendChild(epicGun_text_creator);
 
@@ -154,7 +180,12 @@ class CreateMethod{
         //__legendGun text
         var legendGun_text_creator = document.createElement("p");
         legendGun_text_creator.setAttribute("id", "legendGun-text");
-        legendGun_text_creator.innerText = "گان لجند :" + " " + this.legendGun;
+        if(this.legendGun == ""){
+            this.legendGun = 0;
+            legendGun_text_creator.innerText = "گان لجند :" + " " + this.legendGun;
+        } else {
+            legendGun_text_creator.innerText = "گان لجند :" + " " + this.legendGun;
+        }
 
         legendGun_frame_creator.appendChild(legendGun_text_creator);
 
@@ -166,20 +197,29 @@ class CreateMethod{
         //__price text
         var price_text_creator = document.createElement("p");
         price_text_creator.setAttribute("id", "price-text");
-        price_text_creator.innerText = "قیمت :" + " " + this.price;
+        if(this.price == ""){
+            this.price = 0;
+            price_text_creator.innerText = "قیمت :" + " " + this.price + " " + "هزار تومان";
+        } else {
+            price_text_creator.innerText = "قیمت :" + " " + this.price + " " + "هزار تومان";
+        }
 
         price_frame_creator.appendChild(price_text_creator);
 
         //append to header
-        header.appendChild();
-        header.appendChild();
-        header.appendChild();
+        header.appendChild(logo_frame_creator);
+        header.appendChild(code_frame_creator);
+        header.appendChild(rigen_frame_creator);
 
         //append to footer
-        footer.appendChild();
-        footer.appendChild();
+        footer.appendChild(legendGun_frame_creator);
+        footer.appendChild(epicGun_frame_creator);
+        footer.appendChild(battlePass_frame_creator);
+        footer.appendChild(price_frame_creator);
         
         //add style method
-        method_style.setAttribute("href", this.style);
+        var style_element = document.getElementById("method-style");
+        style_element.setAttribute("href", this.style);
+
     }
 }
